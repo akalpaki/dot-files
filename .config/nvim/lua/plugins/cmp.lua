ca6243567c1da6cc2d70138cfc7b7c76d9470c1e
akalpaki <akalpaki@proton.me>
2025-08-18 00:27:52 +0300

    going back to old known friends

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,35 @@
+return {
+	'hrsh7th/nvim-cmp',
+	event = 'InsertEnter',
+	dependencies = {
+		{
+		'L3MON4D3/LuaSnip',
+		build = (function ()
+			return 'make install_jsregexp'
+		end)(),
+		},
+		'saadparwaiz1/cmp_luasnip',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-path',
+	},
+	config = function ()
+		local cmp = require 'cmp'
+		local luasnip = require 'luasnip'
+		luasnip.config.setup {}
+
+		cmp.setup {
+			snippet = {
+				expand = function (args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			completion = { completeopt = 'menu,menuone,noinsert' },
+			mapping = cmp.mapping.preset.insert {
+				['<C-n>'] = cmp.mapping.select_next_item(),
+				['<C-p>'] = cmp.mapping.select_prev_item(),
+				['<CR>'] = cmp.mapping.confirm { select = true },
+				['<C-Space>'] = cmp.mapping.complete {},
+			}
+		}
+	end
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,5 +2,19 @@ return {
 	'neovim/nvim-lspconfig',
 	config = function()
 		vim.lsp.enable({'lua_ls', 'gopls', 'tinymist'})
+
+		vim.api.nvim_create_autocmd('LspAttach', {
+			group = vim.api.nvim_create_augroup('lsp-attach-setup', {clear = true}),
+			callback = function(event)
+				local map = vim.keymap.set
+				local opts = { buffer = event.buf }
+
+				map('n', 'gd', require('telescope.builtin').lsp_definitions, opts)
+				map('n', 'gI', require('telescope.builtin').lsp_implementations, opts)
+				map('n', 'gr', require('telescope.builtin').lsp_references, opts)
+				map('n', '<leader>r', vim.lsp.buf.rename, opts)
+				map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+			end,
+		})
 	end
 }
