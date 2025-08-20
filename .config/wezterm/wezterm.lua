17629aeced81404bd4dbeb29b207819c0858d59c
akalpaki <akalpaki@proton.me>
2025-08-20 22:32:43 +0300

    merging updates from my work setup

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,5 +10,6 @@ config.color_scheme = 'Apple Classic'
 config.font_size = 18
 config.font = wezterm.font('DepartureMono Nerd Font')
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.default_cursor_style = 'SteadyBlock'
 
 return config
